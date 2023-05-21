48c9cf0619726e137dd691a25e036e4fb9a9105c
GamJaDo <kim0615157@gmail.com>
2023-05-22 01:36:29 +0900

    no message

---
+++ 가중치_그래프/Floyd.py
@@ -0,0 +1,40 @@
+INF = 9999
+
+def printA(A):
+    vsize = len(A)
+
+    print("====================================")
+    for i in range(vsize):
+        for j in range(vsize):
+            if A[i][j] == INF:
+                print(" INF ", end = '')
+            else:
+                print("%4d " %(A[i][j]), end = '')
+
+        print()
+
+def shortest_path_floyd(vertex, adj):
+    vsize = len(vertex)
+    A = list(adj)
+
+    for i in range(vsize):
+        A[i] = list(adj[i])
+
+    for k in range(vsize):
+        for i in range(vsize):
+            for j in range(vsize):
+                if A[i][k]+A[k][j] < A[i][j]:
+                    A[i][j] = A[i][k]+A[k][j]
+        printA(A)
+
+vertex = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+weight = [[0, 7, INF, INF, 3, 10, INF],
+          [7, 0, 4, 10, 2, 6, INF],
+          [INF, 4, 0, 2, INF, INF, INF],
+          [INF, 10, 2, 0, 11, 9, 4],
+          [3, 2, INF, 11, 0, INF, 5],
+          [10, 6, INF, 9, INF, 0, INF],
+          [INF, INF, INF, 4, 5, INF, 0],]
+
+print("Shortest Path By Floyd's Algorithm")
+shortest_path_floyd(vertex, weight)
